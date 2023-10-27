5d04114098824ac5c9bebc5ed066d6eea9936369
rikufkd <148538256+rikufkd@users.noreply.github.com>
2023-10-27 19:36:54 +0900

    Add files via upload

---
+++ 課題１.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	int a, b;
+	a = 0;
+	b = 0;
+	printf_s("D‚«‚È”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+	scanf_s("%d", &a);
+	b = a * a;
+	printf_s("“ü—Í‚µ‚½”š‚Ì“ñæ‚Í%d‚Å‚·B", b);
+	return 0;
+}
