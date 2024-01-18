2f08034643e3ba6c885a6ece725e592f5f3056a1
rikufkd <148538256+rikufkd@users.noreply.github.com>
2024-01-18 12:51:24 +0900

    Add files via upload

--- 課題１.c
+++ 課題１.c
@@ -1,12 +1,46 @@
 #include<stdio.h>
+#include<math.h>
 int main(void)
 {
-	int a, b;
+	int a, b, c, d;
 	a = 0;
 	b = 0;
-	printf_s("好きな数字を入力してください。");
+	c = 0;
+	printf("aの値を入力してください");
 	scanf_s("%d", &a);
-	b = a * a;
-	printf_s("入力した数字の二乗は%dです。", b);
+	if (a==0)
+	{
+		printf("0以外の数字を入力してください");
+		return 0;
+	}
+	printf("bの値を入力してください");
+	scanf_s("%d", &b);
+	printf("cの値を入力してください");
+	scanf_s("%d", &c);
+	d = (b * b) - 4 * a * c;
+	if (d < 0)
+	{
+		printf("解なし");
+	}
+	double y1 = 0;
+	double y2 = 2 * a;
+	double y3 = 0;
+	double y4 = sqrt(d);
+	double y5 = 0;
+	double y6 = 0;
+	y1 = -b + y4;
+	y5 = -b - y4;
+	y3 = y1 / y2;
+	y6 = y5 / y2;
+	d = (b * b) - 4 * a * c;
+	if (d == 0)
+	{
+		printf("y=%f", y3);
+	}
+	if (d > 0)
+	{
+		printf("y=%f,y=%f", y3, y6);
+	}
+
 	return 0;
 }
